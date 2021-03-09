ce4b391c8c9eb027c4a8bcd50c9bfd8f7c53e187
phuchp1997 <phuchp1997@gmail.com>
2021-03-09 14:32:05 -0500

    update

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-export const MONGOOSEDB = 'mongodb+srv://lover-network:Y3QavYjsFwdQqkI4@cluster0.kjjyo.mongodb.net/lover-network?retryWrites=true&w=majority';
-export const SECRET_KEY = 'some very secret key';
-export const API_KEY_SENDGRID = "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";
-export const CLIENT_URL = "localhost:5000";
+module.exports = {
+    MONGOOSEDB: 'mongodb+srv://lover-network:Y3QavYjsFwdQqkI4@cluster0.kjjyo.mongodb.net/lover-network?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key',
+    API_KEY_SENDGRID: "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",
+    CLIENT_URL: "localhost:5000"
+};
